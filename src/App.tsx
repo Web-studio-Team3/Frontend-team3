3b6a264e657e6d97e32ef0b9a994c74923cd9ed2
Sa888nO <shura.reno.97@bk.ru>
2022-10-06 19:51:38 +0300

    created project st-e and started routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from "react";
-
-import styles from "./App.module.scss";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 function App() {
-  return <div className={styles.App}></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<>main</>} />
+        <Route path="/account" element={<>acc</>} />
+        <Route path="/market" element={<>market</>} />
+        <Route path="/market-item" element={<>market-item</>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
