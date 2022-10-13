bfead6d570b211b9e687a2900ca4a169466b4e53
Sa888nO <shura.reno.97@bk.ru>
2022-10-13 12:14:25 +0300

    Error page was done

--- src/components/userData/UserDataItem/UserDataItem.jsx
+++
@@ -1,2 +0,0 @@
-const UserDataItem = () => {};
-export default UserDataItem;
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,11 +1,19 @@
 import { LogoIcon } from "@assets/icons/Icons";
+import classNames from "classnames";
 import styles from "./Error.module.scss";
 
 const Error = () => {
 	return (
-		<main className={styles["error__body"]}>
-			<div className={"container"}>
+		<main className={styles["error"]}>
+			<div className={styles["error__body"]}>
 				<LogoIcon />
+				<p className={styles["error__warning-text"]}>404</p>
+				<div className={styles["information"]}>
+					<span className={styles["information__line"]}></span>
+					<p className={styles["information__text"]}>
+						Такой страницы пока нет, но есть много других
+					</p>
+				</div>
 			</div>
 		</main>
 	);
