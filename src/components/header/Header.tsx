2e4d3ba890fdb05740022b1f721b943acbcc8e45
Павел <58857483+Haskiro@users.noreply.github.com>
2022-11-07 18:58:17 +0300

    new header is ready

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,74 +11,85 @@ import styles from "./Header.module.scss";
 
 const Header = (): JSX.Element => {
 	return (
-		<header className={classNames(styles.section, "container")}>
-			<Link to="/" className={styles.logo}>
-				<LogoIcon />
-			</Link>
-			<form method="POST" className={styles.form}>
-				<button
-					type="button"
-					className={classNames(styles.button, styles.buttonCategory)}
-				>
-					<CategoryIcon />
-				</button>
-				<div className={styles.search}>
-					<input
-						type="text"
-						className={styles.input}
-						placeholder="Поиск по объялениям"
-					/>
+		<header className="container">
+			<div className={styles.section}>
+				<Link to="/" className={styles.logo}>
+					<LogoIcon />
+				</Link>
+				<form method="POST" className={styles.form}>
 					<button
 						type="button"
 						className={classNames(
 							styles.button,
-							styles.buttonSearch
+							styles.buttonCategory
 						)}
 					>
-						<SearchIcon />
+						<CategoryIcon />
+						<p className={styles.buttonText}>Категории</p>
 					</button>
-				</div>
-			</form>
-			<ul className={styles.menu}>
-				<li className={styles.item}>
-					<Link
-						to="/#"
-						className={classNames(styles.link, styles.linkCreateAd)}
-					>
-						Разместить объявление
-					</Link>
-				</li>
-				<li className={styles.item}>
-					<NavLink
-						to="/account/me/favorites"
-						className={({ isActive }) =>
-							isActive
-								? classNames(
-										styles.linkIcon,
-										styles.linkIconActive
-								  )
-								: styles.linkIcon
-						}
-					>
-						<HeartIcon />
-					</NavLink>
-				</li>
-				<li className={styles.item}>
-					<NavLink
-						to="/account/me"
-						className={({ isActive }) =>
-							isActive
-								? classNames(
-										styles.linkIcon,
-										styles.linkIconActive
-								  )
-								: styles.linkIcon
-						}
-					>
-						<ProfileIcon />
-					</NavLink>
-				</li>
-			</ul>
+					<div className={styles.search}>
+						<input
+							type="text"
+							className={styles.input}
+							placeholder="Поиск по объялениям"
+						/>
+						<button
+							type="button"
+							className={classNames(
+								styles.button,
+								styles.buttonSearch
+							)}
+						>
+							<SearchIcon />
+						</button>
+					</div>
+				</form>
+				<ul className={styles.menu}>
+					<li className={styles.item}>
+						<Link
+							to="/#"
+							className={classNames(
+								styles.link,
+								styles.linkCreateAd
+							)}
+						>
+							Разместить объявление
+						</Link>
+					</li>
+					<li className={styles.item}>
+						<NavLink
+							to="/account/me/favorites"
+							className={({ isActive }) =>
+								isActive
+									? classNames(
+											styles.linkIcon,
+											styles.linkIconActive
+									  )
+									: styles.linkIcon
+							}
+						>
+							<HeartIcon />
+							<p className={styles.linkText}>Избранное</p>
+						</NavLink>
+					</li>
+					<li className={styles.item}>
+						<NavLink
+							to="/account/me"
+							className={({ isActive }) =>
+								isActive
+									? classNames(
+											styles.linkIcon,
+											styles.linkIconActive
+									  )
+									: styles.linkIcon
+							}
+						>
+							<ProfileIcon />
+							<p className={styles.linkText}>Профиль</p>
+						</NavLink>
+					</li>
+				</ul>
+			</div>
 		</header>
 	);
 };
