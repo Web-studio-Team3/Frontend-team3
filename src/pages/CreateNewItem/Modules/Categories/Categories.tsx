4c2d249ee2411a2d9e9b8c523c2083e458c6f5ff
Alex E <egorushkin.a.s@mail.ru>
2023-05-10 18:14:50 +0300

    fake create new items

---
+++ src/pages/CreateNewItem/Modules/Categories/Categories.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import CategoryItem from "../CategoryItem";
+
+const categories: {
+	title: string;
+	childrens: string[] | null;
+}[] = [
+	{ title: "Личные вещи", childrens: null },
+	{ title: "Электроника", childrens: null },
+	{ title: "Для дома и дачи", childrens: null },
+	{ title: "Запчасти и аксессуары", childrens: null },
+	{ title: "Хобби и отдых", childrens: null },
+];
+
+type CategoriesProps = {
+	currentCategory: string;
+	actionCategory: (category: string) => void;
+};
+
+const CategoriesBlock = styled.div`
+	flex: none;
+	margin-top: 20px;
+	border: 1px solid #72757e;
+	border-radius: 30px;
+	padding: 18px;
+	display: flex;
+	flex-direction: column;
+	gap: 12px;
+
+	h5 {
+		font-family: "Ubuntu";
+		font-size: 21px;
+		color: white;
+		font-weight: bold;
+	}
+`;
+
+const Categories: React.FC<CategoriesProps> = ({
+	currentCategory,
+	actionCategory,
+}) => {
+	return (
+		<CategoriesBlock>
+			<h5>Категории</h5>
+			{categories.map((category) => (
+				<CategoryItem
+					key={category.title}
+					text={category.title}
+					currentCategory={currentCategory}
+					onClick={() => {
+						actionCategory(category.title);
+					}}
+				/>
+			))}
+		</CategoriesBlock>
+	);
+};
+
+export default Categories;
--- src/pages/CreateNewItem/Modules/Content/Content.tsx
+++ src/pages/CreateNewItem/Modules/Content/Content.tsx
@@ -1,14 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+// import Categories from "@pages/market/components/Categories/Categories";
 import { Form, Input } from "antd";
+import Categories from "../Categories/Categories";
 import styles from "./Content.module.scss";
 
 export const Content = () => {
+	const [category, setCategory] = useState<string | null>(null);
 	return (
 		<div className={styles.wrapper}>
+			<div className={styles.title}>Размещение объявления</div>
 			<Form>
-				<Form.Item>
-					<Input />
-				</Form.Item>
+				{/* <Form.Item> */}
+				<div className={styles.categoriesBlock}>
+					<div
+						style={{
+							color: "#ffffff",
+							fontSize: 14,
+						}}
+					>
+						Выберете категорию
+					</div>
+					<Categories
+						actionCategory={(e) => console.log(e)}
+						currentCategory="dddd"
+					/>
+				</div>
+				<Input />
+				{/* </Form.Item> */}
 			</Form>
 		</div>
 	);
