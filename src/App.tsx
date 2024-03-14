3a35669052146ca74a15ff4c1a234478dac20191
TsInella <firefly.goo5@gmail.com>
2024-03-14 13:00:24 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,8 @@ export const BreadcrumbsContext = createContext<IBreadcrumbsContext | null>(
 function App() {
 	const [breadcrumbs, setBreadcrumbs] = useState<Record<string, string>>({});
 	const dispatch = useDispatch();
-	const [pageLoading,setPageLoading] = useState(true);
+	const [pageLoading, setPageLoading] = useState(true);
+
 	useLayoutEffect(() => {
 		window.addEventListener('load', () => {
 			setTimeout(() => {
@@ -50,90 +51,38 @@ function App() {
 		};
 	}, []);
 
-
 	const authData = useSelector((state: RootState) => state.Auth);
 
-	if(pageLoading) return <div className={styles.preloader}>
-		<div className={styles.loader}>
-		</div>
-	</div>
+	if (pageLoading) {
+		return (
+			<div className={styles.preloader}>
+				<div className={styles.loader}></div>
+			</div>
+		);
+	}
+
 	return (
-		<BreadcrumbsContext.Provider
-			value={{
-				breadcrumbs,
-				setBreadcrumbs,
-			}}
-		>
+		<BreadcrumbsContext.Provider value={{ breadcrumbs, setBreadcrumbs }}>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Market />} />
 					<Route path="/advert/:id" element={<MarketItemPage />} />
-					<Route
-						path="/account/me/"
-						element={
-							<AccountLayout
-								type="my"
-								component={<Account type="my" />}
-							/>
-						}
-					>
+					<Route path="/account/me/" element={<AccountLayout type="my" component={<Account type="my" />} />}>
 						<Route index element={<AccountSettings />} />
 						<Route path="messages" element={<AccountMessages />} />
-						<Route
-							path="messages/:id"
-							element={
-								<AccountSingleChat
-									user={{
-										first_name: "Егор",
-										last_name: "Сашев",
-										was_active_at: "4:20",
-									}}
-								/>
-							}
-						/>
-						<Route
-							path="favorites"
-							element={
-								<AccountAds
-									type="favorite"
-									title="Избранные объявления"
-									subtitle="Здесь представленны все товары, которые тебе понравились"
-								/>
-							}
-						/>
+						<Route path="messages/:id" element={<AccountSingleChat user={{ first_name: "Егор", last_name: "Сашев", was_active_at: "4:20" }} />} />
+						<Route path="favorites" element={<AccountAds type="favorite" title="Избранные объявления" subtitle="Здесь представлены все товары, которые тебе понравились" />} />
 						<Route path="reviews" element={<AccountReviews />} />
-						<Route
-							path="my-ads"
-							element={
-								<AccountAds type="my" title="Мои объявления" />
-							}
-						/>
+						<Route path="my-ads" element={<AccountAds type="my" title="Мои объявления" />} />
 					</Route>
-					<Route
-						path="/account/id/"
-						element={
-							<AccountLayout
-								type="another"
-								component={<Account type="another" />}
-							/>
-						}
-					>
-						<Route
-							index
-							element={
-								<AccountAds type="my" title="Объявления" />
-							}
-						/>
+					<Route path="/account/id/" element={<AccountLayout type="another" component={<Account type="another" />} />}>
+						<Route index element={<AccountAds type="my" title="Объявления" />} />
 						<Route path="reviews" element={<AccountReviews />} />
 					</Route>
 					<Route path="*" element={<Error />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/registration" element={<Registration />} />
-					<Route path="/login" element={<Registration />} />
-					<Route
-						path="/create-new-item-page"
-						element={<CreateNewItem />}
-					/>
+					<Route path="/create-new-item-page" element={<CreateNewItem />} />
 				</Routes>
 			</BrowserRouter>
 		</BreadcrumbsContext.Provider>
